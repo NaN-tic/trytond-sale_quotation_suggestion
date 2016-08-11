eacf9795106df0bb74dfd412dc96d80cff0fd5d6
Albert Cervera i Areny <albert@nan-tic.com>
2016-08-11 17:57:34 +0200

    Adapt to version 4.0.

--- sale.py
+++ sale.py
@@ -1,8 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelSQL, ModelView, fields
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Id, If, Eval, Bool
+from trytond.model import fields
+from trytond.pool import PoolMeta
 
 __all__ = ['Sale']
 
@@ -12,6 +11,7 @@ class Sale:
     __metaclass__ = PoolMeta
     suggested_quotations = fields.Function(fields.Char(
             'Suggested Quotations'), 'on_change_with_suggested_quotations')
+    suggested_for = fields.Many2Many
 
     @fields.depends('party')
     def on_change_with_suggested_quotations(self, name=None):
